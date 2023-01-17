69c4e214ea0ce7f77ec2360e4e010b620fe891cd
Elijah-glitch <62041760+Elijah-glitch@users.noreply.github.com>
2023-01-17 16:34:28 -0500

    Update music.ts

--- src/configs/music.ts
+++ src/configs/music.ts
@@ -5,6 +5,12 @@ const music: MusicData = {
   artist: "Post Malone / Swae Lee",
   cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
   audio: "music/sunflower.mp3"
+const music: MusicData = {
+  title: "sad tiktok",
+  artist: "elijah mix",
+  cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
+  audio: "music/sad tiktok.mp3"
+};
 };
 
 export default music;
